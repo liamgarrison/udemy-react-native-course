ed846401518d8b138122936ac65d4a4d1f489849
Liam Garrison <liamgarrison@gmail.com>
2019-06-07 15:01:18 +0100

    basic fetch for albums in albumlist

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class AlbumList extends Component {
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+  render() {
+    return (
+      <View>
+        <Text>
+          Album List!!!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
