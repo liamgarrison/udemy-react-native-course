258f7f1637982a54c6cd55fa69548ff5c9072db9
Liam Garrison <liamgarrison@gmail.com>
2019-06-10 13:11:15 +0100

    added album artwork to card

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,19 +4,28 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { textContainer, thumbnailImage } = styles;
-  
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    textContainer,
+    thumbnailImage,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image source={{ uri: thumbnail_image }} style={thumbnailImage} />
         </View>
         <View style={textContainer}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
-          <Text>{title}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -31,6 +40,21 @@ const styles = {
     height: 50,
     width: 50,
   },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  headerTextStyle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  }
 };
 
 export default AlbumDetail;
