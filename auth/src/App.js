1b20b75ab6290118fbb8b282773ea592a6995baf
Liam Garrison <liamgarrison@gmail.com>
2019-06-11 09:44:35 +0100

    add firebase config

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common'
 
 class App extends Component {
-  render() {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAyeT1JAUZkKwmLHVtdwKNTn_tcFEGtYpg",
+      authDomain: "udemy-authentication-57561.firebaseapp.com",
+      databaseURL: "https://udemy-authentication-57561.firebaseio.com",
+      projectId: "udemy-authentication-57561",
+      storageBucket: "udemy-authentication-57561.appspot.com",
+      messagingSenderId: "865442463284",
+      appId: "1:865442463284:web:f72cdc00123f97a6"
+    })
+  }  
+
+ render() {
     return (
       <View>
         <Header headerText={"Authentication"}></Header>
